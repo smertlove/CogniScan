818b8ba0aac2a9e5adc217e3fcdc70daac2d947c
Kirill Soloshenko <k.soloshenko1@mts.ai>
2025-10-30 02:24:41 +0300

    add simple index implementation and its demo

---
+++ CogniScan/index.py
@@ -0,0 +1,39 @@
+import faiss
+import numpy as np
+
+from .encoder import Encoder
+
+
+class RamIndex:
+
+    def __init__(self,):
+        self.index = None
+        self.texts = []
+        self.meta = []
+
+    def build_index(self, texts, meta, encoder: Encoder):
+        embeddings = encoder.encode(texts)
+        self.index = faiss.IndexFlatIP(embeddings.shape[1])
+        self.normalize(embeddings)
+        self.index.add(embeddings.astype(np.float32))
+        self.texts = texts
+        self.meta = meta
+
+    def normalize(self, x):
+        return faiss.normalize_L2(x)
+
+    def search(self, query: str, encoder: Encoder, k=3):
+
+        query_embedding = encoder.encode([query])
+        self.normalize(query_embedding)
+
+        distances, indices = self.index.search(query_embedding.astype(np.float32), k)
+
+        results = []
+        for _, i in zip(distances[0], indices[0]):
+            results.append({
+                "text": self.texts[i],
+                "meta": self.meta[i]
+            })
+
+        return results
