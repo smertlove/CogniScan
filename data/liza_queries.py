3b62a058694cbc6f0a9599ee962fb07f30d6889a
e-plakutina <plakutina2002@gmail.com>
2025-11-13 19:38:25 +0300

    Add files via upload

---
+++ data/liza_queries.py
@@ -0,0 +1,164 @@
+QUERY_1 = """
+(
+    mental disorders
+    OR
+    behavioural disorders
+    OR
+    psychiatric disorders
+    OR
+    mental illness
+)
+AND
+(
+    English[la]
+    OR
+    English[ad]
+    OR
+    USA
+    OR
+    english[lang]
+    OR
+    UK
+)
+""".strip()
+
+
+QUERY_2 = """
+(
+    anxiety disorders
+    OR
+    anxiety
+    OR
+    GAD
+)
+AND
+(
+    English[la]
+    OR
+    English[ad]
+    OR
+    USA
+    OR
+    english[lang]
+    OR
+    UK
+)
+""".strip()
+
+
+
+QUERY_3 = """
+(
+    obsessive-compulsive disorder
+    OR
+    OCD
+)
+AND
+(
+    English[la]
+    OR
+    English[ad]
+    OR
+    USA
+    OR
+    english[lang]
+    OR
+    UK
+)
+""".strip()
+
+
+
+QUERY_4 = """
+(
+    trauma and stressor related disorders
+    OR
+    PTSD
+)
+AND
+(
+    English[la]
+    OR
+    English[ad]
+    OR
+    USA
+    OR
+    english[lang]
+    OR
+    UK
+)
+""".strip()
+
+
+
+QUERY_5 = """
+(
+    personality disorders
+    OR
+    borderline personality
+    OR
+    antisocial personality
+)
+AND
+(
+    English[la]
+    OR
+    English[ad]
+    OR
+    USA
+    OR
+    english[lang]
+    OR
+    UK
+)
+""".strip()
+
+
+
+QUERY_6 = """
+(
+    impulse control disorders
+    OR
+    impulse control
+    OR
+    pathological gambling
+    OR
+    kleptomania
+    OR
+    pyromania
+)
+AND
+(
+    English[la]
+    OR
+    English[ad]
+    OR
+    USA
+    OR
+    english[lang]
+    OR
+    UK
+)
+""".strip()
+
+
+
+QUERY_7 = """
+(
+    paraphilic disorders
+    OR
+    sexual preference disorders
+)
+AND
+(
+    English[la]
+    OR
+    English[ad]
+    OR
+    USA
+    OR
+    english[lang]
+    OR
+    UK
+)
+""".strip()
