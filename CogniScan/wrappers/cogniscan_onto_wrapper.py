023db0bbb3cc91f94a327bbb1dbe870390127546
Kirill Soloshenko <k.soloshenko1@mts.ai>
2025-12-12 20:14:21 +0300

    add describe

--- CogniScan/wrappers/cogniscan_onto_wrapper.py
+++ CogniScan/wrappers/cogniscan_onto_wrapper.py
@@ -1,4 +1,6 @@
 from rdflib import Graph
+from rdflib import RDF, OWL
+
 import re
 
 
@@ -33,6 +35,23 @@ class CogniScanOntoWrapper:
             <{}> <{}> ?description .
         }}}}
         """.format("{}", self.has_description_uri)
+
+
+        stats = self.describe()
+        max_key_len = max(len(str(k)) for k in stats.keys())
+        max_val_len = max(len(str(v)) for v in stats.values())
+
+        line_width = max_key_len + max_val_len + 10
+
+        print("=" * line_width)
+        print(f"ONTOLOGY STATISTICS".center(line_width))
+        print("=" * line_width)
+        
+        for key, value in stats.items():
+            display_name = key.replace('_', ' ').title()
+            print(f"{display_name:<{max_key_len+2}}:{value:>{max_val_len+3}}")
+        
+        print("=" * line_width)
 
     def uri2name(self, uri: str):
         uri_str = str(uri)
@@ -80,5 +99,18 @@ class CogniScanOntoWrapper:
         query = self._q_description_of_disease.format(disease_uri)
         return self.query_my_onto(query)
 
+    def describe(self):
 
+        classes = list(self.g.subjects(RDF.type, OWL.Class))
+        object_properties = list(self.g.subjects(RDF.type, OWL.ObjectProperty))
+        datatype_properties = list(self.g.subjects(RDF.type, OWL.DatatypeProperty))
+        individuals = list(self.g.subjects(RDF.type, OWL.NamedIndividual))
+
+        return {
+            "classes"            : len(classes),
+            "object_properties"  : len(object_properties),
+            "datatype_properties": len(datatype_properties),
+            "individuals"        : len(individuals),
+            "total_statements"   : len(self.g),
+        }
 
